adb2e9bed68e1afceff08479c79b3f4511085579
odilhon85 <i.o.r@inbox.ru>
2022-07-17 10:43:40 +0500

    Changes done 1-attempt

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        int letter = character;
+        if(letter>=65 && letter<=90 || letter>=97 && letter<=122){
+            if(character=='a' || character=='A' || character=='e' || character=='E' ||
+                    character=='u' || character=='U' || character=='i' || character=='I' ||
+                    character=='o' || character=='O'){
+                System.out.println("Vowel");
+            }else{
+                System.out.println("Consonant");
+            }
+        }else{
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        //65-90 A-Z; 97-122 a-z
+        int letter = symbol;
+        if(letter>=65 && letter<=90 || letter>=97 && letter<=122){
+            System.out.println("English");
+        }else{
+            System.out.println("Non English");
+        }
     }
 }
